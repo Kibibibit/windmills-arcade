c4d7a860cb490fcaf7b6924ae93188de1d970481
Dan M <daniel@thewindmills.com.au>
2022-12-05 12:51:15 +1100

    Updated root with switch game method

--- scripts/RootObject.gd
+++ scripts/RootObject.gd
@@ -8,11 +8,18 @@ extends Node2D
 
 # This is the root of the entire application
 
-var game_resource
 var game_instance
 
-# Called when the node enters the scene tree for the first time.
+onready var games = {
+	"MainMenu":load("res://scenes/games/MainMenu.tscn"),
+	"Sawayama":load("res://scenes/games/Sawayama.tscn")
+}
+
 func _ready():
-	game_resource = load("res://scenes/games/Sawayama.tscn")
-	game_instance = game_resource.instance()
+	switch_game("MainMenu")
+
+func switch_game(game_name:String):
+	if (game_instance != null):
+		remove_child(game_instance)
+	game_instance = games[game_name].instance()
 	add_child(game_instance)
---
+++ scripts/games/MainMenu.gd
@@ -0,0 +1,7 @@
+class_name MainMenu
+extends GameParent
+
+
+func _ready():
+	
+	self.load_textures()
